402158414329d594fa0a433519ac4648f03b3e7d
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-01-28 20:40:19 +0530

    next steps

---
+++ next_steps.py
@@ -0,0 +1,15 @@
+#Wrapping steps
+
+#Source Control: Github
+
+"""
+Source control lets you track changes to your project.
+You can use that to your advantage to commit early and often and track changes to your project.
+link: https://try.github.io/
+"""
+
+#Next Steps
+"""
+free Book: https://docs.python-guide.org/
+For learning the A-Z of Python web apps:  https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+"""
